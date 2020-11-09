b8cc1b2242956dc41e003b81d75a8cbb19510d0b
shakeel <shakeelkoper@gmail.com>
2020-11-09 10:49:50 +0530

    replacing redux with context api

--- src/containers/Products.js
+++ src/containers/Products.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useContext } from "react";
+import { ProductsContext } from "../context/products-context";
+import ProductItem from "../components/Products/ProductItem";
+import "./Products.css";
 
-import ProductItem from '../components/Products/ProductItem';
-import './Products.css';
+const Products = (props) => {
+	const productList = useContext(ProductsContext).products;
 
-const Products = props => {
-  const productList = useSelector(state => state.shop.products);
-  return (
-    <ul className="products-list">
-      {productList.map(prod => (
-        <ProductItem
-          key={prod.id}
-          id={prod.id}
-          title={prod.title}
-          description={prod.description}
-          isFav={prod.isFavorite}
-        />
-      ))}
-    </ul>
-  );
+	return (
+		<ul className="products-list">
+			{productList.map((prod) => (
+				<ProductItem
+					key={prod.id}
+					id={prod.id}
+					title={prod.title}
+					description={prod.description}
+					isFav={prod.isFavorite}
+				/>
+			))}
+		</ul>
+	);
 };
 
 export default Products;
--- src/index.js
+++ src/index.js
@@ -1,24 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { combineReducers, createStore } from 'redux';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import ProductsProvider from "./context/products-context";
+import { BrowserRouter } from "react-router-dom";
 
-import './index.css';
-import App from './App';
-import productReducer from './store/reducers/products';
-
-const rootReducer = combineReducers({
-  shop: productReducer
-});
-
-const store = createStore(rootReducer);
+import "./index.css";
+import App from "./App";
 
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
-  document.getElementById('root')
+	<ProductsProvider>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</ProductsProvider>,
+	document.getElementById("root")
 );
